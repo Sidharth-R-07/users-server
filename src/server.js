e922818c71ed45d304907e8cbfaad031d1439c6a
Sidharth-R-07 <sidhuram365@gmail.com>
2026-01-09 21:58:15 +0530

    Added Production URL

--- src/server.js
+++ src/server.js
@@ -8,14 +8,27 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://users-client-nu.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      // allow requests with no origin (Postman, server-to-server)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
-
 const PORT = process.env.PORT || 5000;
 
 app.use("/users", userRoutes);
